65dd092911eae11e8e8ba36bbb05556b6c06f255
Olha-Boko <o_boiko@fanshaweonline.ca>
2020-02-29 20:00:43 -0500

    adding basic socket stuff

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,16 @@
 // imports always go first - if we're importing anything
+
+const socket = io();
+
+//the packet is whatever data we use send through with the connection event from the server
+function setUserId(packet) {
+    
+    console.log(packet);
+}
+
+function showDisconnectMessage() {
+    console.log('a user disconnected');
+}
+
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('disconnect', showDisconnectMessage)
